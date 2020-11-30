9145a58ded03f3366ee7db0b9bd1028724a9329e
YazminTrucido <69005528+YazminTrucido@users.noreply.github.com>
2020-11-30 12:52:56 -0300

    Entrega 1

---
+++ js/product-info.js
@@ -0,0 +1,30 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+
+    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            product = resultObj.data;
+
+            let productNameHTML  = document.getElementById("name");
+            let productDescriptionHTML = document.getElementById("description");
+            let productCostHTML = document.getElementById("cost");
+            let productCurrencyHTML = document.getElementById("currency");
+            let productSoldCountHTML = document.getElementById("soldCount");
+            let productCategoryHTML = document.getElementById("category");
+            
+            productNameHTML.innerHTML = product.name;
+            productDescriptionHTML.innerHTML = product.description;
+            productCostHTML.innerHTML = product.cost;
+            productCurrencyHTML.innerHTML = product.currency;
+            productSoldCountHTML.innerHTML = product.soldCount;
+            productCategoryHTML.innerHTML = product.category;
+
+            //Muestro las imagenes en forma de galería
+            showImagesGallery(product.images);
+        }
+    });
+
+});
